fa793b65ae011ec5de9f466e841388212c689e8e
Pourya MandiSanam <poury.ms@gmail.com>
2019-09-14 22:44:43 +0430

    add id=3408

---
+++ solution_3408.py
@@ -0,0 +1,7 @@
+# سوال برنامه نویسی برنامه نویسی سوال
+# https://quera.ir/problemset/contest/3408/سؤال-سوال-برنامه-نویسی-برنامه-نویسی-سوال
+# pourya.2374@gmail.com
+
+input()
+result = input().split(' ')
+print(*result[::-1], sep=' ')
